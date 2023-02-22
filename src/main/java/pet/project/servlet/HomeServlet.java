93f103a161195b0d88d71842bcf2ba174fc1b047
Vladimir Kidyaev <krios2146@gmail.com>
2023-02-22 20:02:52 +0700

    feat: add HomeServlet

---
+++ src/main/java/pet/project/servlet/HomeServlet.java
@@ -0,0 +1,49 @@
+package pet.project.servlet;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.thymeleaf.ITemplateEngine;
+import org.thymeleaf.context.IWebContext;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.web.servlet.IServletWebExchange;
+import org.thymeleaf.web.servlet.JakartaServletWebApplication;
+import pet.project.ConfiguredTemplateEngine;
+
+import java.io.IOException;
+
+@WebServlet(name = "HomeServlet", urlPatterns = "/")
+public class HomeServlet extends HttpServlet {
+
+    private final ITemplateEngine templateEngine = ConfiguredTemplateEngine.getInstance();
+    private IWebContext context;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (context == null) {
+            context = buildWebExchange(req, resp);
+        }
+        super.service(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        templateEngine.process("index", context, resp.getWriter());
+    }
+
+    private IWebContext buildWebExchange(HttpServletRequest req, HttpServletResponse resp) {
+        ServletContext servletContext = this.getServletContext();
+        JakartaServletWebApplication application = JakartaServletWebApplication.buildApplication(servletContext);
+        IServletWebExchange webExchange = application.buildExchange(req, resp);
+        return new WebContext(webExchange);
+    }
+}
