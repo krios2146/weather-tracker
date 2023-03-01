2cc63246c38c14b0d304705b24be63cb338ee240
Vladimir Kidyaev <krios2146@gmail.com>
2023-03-01 19:07:23 +0700

    feat: add ApiLocation model that related to api response

---
+++ src/main/java/pet/project/model/api/ApiLocation.java
@@ -0,0 +1,30 @@
+package pet.project.model.api;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class ApiLocation {
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("lat")
+    private Double latitude;
+    @JsonProperty("lon")
+    private Double longitude;
+    @JsonProperty("country")
+    private String country;
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+}
