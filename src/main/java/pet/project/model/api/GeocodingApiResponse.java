c81f4edcacce7e83eff8f4d7eb925f91116c24de
Vladimir Kidyaev <krios2146@gmail.com>
2023-03-01 19:06:47 +0700

    feat: add GeocodingApiResponse model

---
+++ src/main/java/pet/project/model/api/GeocodingApiResponse.java
@@ -0,0 +1,11 @@
+package pet.project.model.api;
+
+import java.util.List;
+
+public class GeocodingApiResponse {
+    private List<ApiLocation> locationList;
+
+    public List<ApiLocation> getLocationList() {
+        return locationList;
+    }
+}
