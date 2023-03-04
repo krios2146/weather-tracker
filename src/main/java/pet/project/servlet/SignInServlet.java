a964133b7b7f930a04d14ea41ee98c87809e6368
Vladimir Kidyaev <krios2146@gmail.com>
2023-03-04 20:03:43 +0700

    feat: add SignInServlet

---
+++ src/main/java/pet/project/servlet/SignInServlet.java
@@ -0,0 +1,72 @@
+package pet.project.servlet;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.thymeleaf.ITemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.web.servlet.IServletWebExchange;
+import org.thymeleaf.web.servlet.JakartaServletWebApplication;
+import pet.project.dao.SessionDao;
+import pet.project.dao.UserDao;
+import pet.project.model.Session;
+import pet.project.model.User;
+import pet.project.util.TemplateEngineUtil;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@WebServlet(name = "SignInServlet", urlPatterns = "/sign-in")
+public class SignInServlet extends HttpServlet {
+
+    private final UserDao userDao = new UserDao();
+    private final SessionDao sessionDao = new SessionDao();
+    private final ITemplateEngine templateEngine = TemplateEngineUtil.getInstance();
+    private WebContext context;
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (context == null) {
+            context = buildWebContext(req, resp);
+        }
+        super.service(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        templateEngine.process("sign-in", context, resp.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+
+        Optional<User> optionalUser = userDao.findByLogin(login);
+        User user = optionalUser.orElseThrow();
+
+        if (!user.getPassword().equals(password)) {
+            throw new RuntimeException("Authorization exception: wrong password");
+        }
+
+        Session session = new Session(user, LocalDateTime.now().plusHours(24));
+        sessionDao.save(session);
+
+        Cookie cookie = new Cookie("sessionId", session.getId().toString());
+        resp.addCookie(cookie);
+
+        resp.sendRedirect(req.getContextPath());
+    }
+
+    private WebContext buildWebContext(HttpServletRequest req, HttpServletResponse resp) {
+        ServletContext servletContext = this.getServletContext();
+        JakartaServletWebApplication application = JakartaServletWebApplication.buildApplication(servletContext);
+        IServletWebExchange webExchange = application.buildExchange(req, resp);
+        return new WebContext(webExchange);
+    }
+}
