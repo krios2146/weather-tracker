6134c1fb02d552e51b53950679070949eaaa82f6
Vladimir Kidyaev <krios2146@gmail.com>
2023-03-10 19:12:06 +0700

    feat: add WeatherDto class

---
+++ src/main/java/pet/project/dto/WeatherDto.java
@@ -0,0 +1,37 @@
+package pet.project.dto;
+
+public class WeatherDto {
+    private Integer id;
+    private String currentState;
+    private String description;
+
+    public WeatherDto(Integer id, String currentState, String description) {
+        this.id = id;
+        this.currentState = currentState;
+        this.description = description;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCurrentState() {
+        return currentState;
+    }
+
+    public void setCurrentState(String currentState) {
+        this.currentState = currentState;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
