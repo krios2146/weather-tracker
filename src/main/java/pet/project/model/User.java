77806e84f537fd61535718f09e85354bd2dc62cf
Vladimir Kidyaev <krios2146@gmail.com>
2023-03-10 19:46:17 +0700

    fix: add custom equals and hashCode to User model

--- src/main/java/pet/project/model/User.java
+++ src/main/java/pet/project/model/User.java
@@ -28,6 +28,26 @@ public class User {
     public User() {
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (!id.equals(user.id)) return false;
+        if (!login.equals(user.login)) return false;
+        return password.equals(user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + login.hashCode();
+        result = 31 * result + password.hashCode();
+        return result;
+    }
+
     public Long getId() {
         return id;
     }
