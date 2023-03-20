4f7938f870382213cd779d1e633e1f58c4ba0c5c
Vladimir Kidyaev <krios2146@gmail.com>
2023-03-20 20:30:43 +0700

    feat: add handling of new env variable to the EntityManagerFactoryUtil

--- src/main/java/pet/project/util/EntityManagerFactoryUtil.java
+++ src/main/java/pet/project/util/EntityManagerFactoryUtil.java
@@ -15,10 +15,14 @@ public class EntityManagerFactoryUtil {
         String jdbcUrl = System.getenv("JAKARTA_PERSISTENCE_JDBC_URL");
         String jdbcUser = System.getenv("JAKARTA_PERSISTENCE_JDBC_USER");
         String jdbcPassword = System.getenv("JAKARTA_PERSISTENCE_JDBC_PASSWORD");
+        String hibernateDdlAuto = System.getenv("HIBERNATE_DDL_AUTO");
 
         config.put("jakarta.persistence.jdbc.url", jdbcUrl);
         config.put("jakarta.persistence.jdbc.user", jdbcUser);
         config.put("jakarta.persistence.jdbc.password", jdbcPassword);
+        if (hibernateDdlAuto != null) {
+            config.put("hibernate.hbm2ddl.auto", hibernateDdlAuto);
+        }
 
         INSTANCE = Persistence.createEntityManagerFactory("weatherPersistenceUnit", config);
     }
