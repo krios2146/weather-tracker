cc3e5fc3dd23967a09bbc06a1b89e8c2ade8b054
Vladimir Kidyaev <krios2146@gmail.com>
2023-03-20 20:27:29 +0700

    feat: add environment variables fetching to the EntityManagerFactoryUtil

--- src/main/java/pet/project/util/EntityManagerFactoryUtil.java
+++ src/main/java/pet/project/util/EntityManagerFactoryUtil.java
@@ -3,11 +3,24 @@ package pet.project.util;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class EntityManagerFactoryUtil {
     private static final EntityManagerFactory INSTANCE;
 
     static {
-        INSTANCE = Persistence.createEntityManagerFactory("weatherPersistenceUnit");
+        Map<String, String> config = new HashMap<>();
+
+        String jdbcUrl = System.getenv("JAKARTA_PERSISTENCE_JDBC_URL");
+        String jdbcUser = System.getenv("JAKARTA_PERSISTENCE_JDBC_USER");
+        String jdbcPassword = System.getenv("JAKARTA_PERSISTENCE_JDBC_PASSWORD");
+
+        config.put("jakarta.persistence.jdbc.url", jdbcUrl);
+        config.put("jakarta.persistence.jdbc.user", jdbcUser);
+        config.put("jakarta.persistence.jdbc.password", jdbcPassword);
+
+        INSTANCE = Persistence.createEntityManagerFactory("weatherPersistenceUnit", config);
     }
 
     public static EntityManagerFactory getInstance() {
