2a63f402dc2fb25a158f3e5cfc8a9548f1be1b44
Vladimir Kidyaev <krios2146@gmail.com>
2023-04-24 12:38:38 +0700

    feat(model): update WeatherApiModel entity with using lombok

--- src/main/java/pet/project/model/api/WeatherApiModel.java
+++ src/main/java/pet/project/model/api/WeatherApiModel.java
@@ -2,25 +2,17 @@ package pet.project.model.api;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
 
+@Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class WeatherApiModel {
     @JsonProperty("id")
     private Integer id;
+
     @JsonProperty("main")
     private String currentState;
+
     @JsonProperty("description")
     private String description;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public String getCurrentState() {
-        return currentState;
-    }
-
-    public String getDescription() {
-        return description;
-    }
 }
