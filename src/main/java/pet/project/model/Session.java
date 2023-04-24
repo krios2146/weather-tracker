b5622d917503506de28cc0b83d8b6d4823bc03f1
Vladimir Kidyaev <krios2146@gmail.com>
2023-04-24 12:36:53 +0700

    feat(model): update Session entity with using lombok

--- src/main/java/pet/project/model/Session.java
+++ src/main/java/pet/project/model/Session.java
@@ -1,10 +1,16 @@
 package pet.project.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "sessions")
 public class Session {
@@ -18,41 +24,8 @@ public class Session {
     @Column(name = "expires_at")
     private LocalDateTime expiresAt;
 
-    public Session(UUID id, User user, LocalDateTime expiresAt) {
-        this.id = id;
-        this.user = user;
-        this.expiresAt = expiresAt;
-    }
-
     public Session(User user, LocalDateTime expiresAt) {
         this.user = user;
-        this.expiresAt = expiresAt;
-    }
-
-    public Session() {
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public LocalDateTime getExpiresAt() {
-        return expiresAt;
-    }
-
-    public void setExpiresAt(LocalDateTime expiresAt) {
         this.expiresAt = expiresAt;
     }
 }
