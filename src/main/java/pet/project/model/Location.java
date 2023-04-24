1257a905a3652a198ba547a66394187f14ae8d7a
Vladimir Kidyaev <krios2146@gmail.com>
2023-04-24 15:00:05 +0700

    feat(model): add unique constraint for Location entity

--- src/main/java/pet/project/model/Location.java
+++ src/main/java/pet/project/model/Location.java
@@ -11,7 +11,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "locations")
+@Table(name = "locations", uniqueConstraints = {@UniqueConstraint(columnNames = {"latitude", "longitude"})})
 public class Location {
     @Id
     @SequenceGenerator(name = "locations_seq", sequenceName = "locations_seq", allocationSize = 1)
