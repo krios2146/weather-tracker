476ad74978e4e0b35d6b679902eeac4202877e9f
Vladimir Kidyaev <krios2146@gmail.com>
2023-04-28 18:09:28 +0700

    feat(dao): add environment variables fetching to the PersistenceUtil

--- src/main/java/pet/project/util/PersistenceUtil.java
+++ src/main/java/pet/project/util/PersistenceUtil.java
@@ -3,11 +3,26 @@ package pet.project.util;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PersistenceUtil {
     private static final EntityManagerFactory ENTITY_MANAGER_FACTORY_INSTANCE;
 
     static {
-        ENTITY_MANAGER_FACTORY_INSTANCE = Persistence.createEntityManagerFactory("weatherPersistenceUnit");
+        Map<String, String> config = new HashMap<>();
+
+        String jdbcUrl = System.getenv("JAKARTA_PERSISTENCE_JDBC_URL");
+        String jdbcUser = System.getenv("JAKARTA_PERSISTENCE_JDBC_USER");
+        String jdbcPassword = System.getenv("JAKARTA_PERSISTENCE_JDBC_PASSWORD");
+        String hibernateDdlAuto = System.getenv("HIBERNATE_DDL_AUTO");
+
+        config.put("jakarta.persistence.jdbc.url", jdbcUrl);
+        config.put("jakarta.persistence.jdbc.user", jdbcUser);
+        config.put("jakarta.persistence.jdbc.password", jdbcPassword);
+        config.put("hibernate.hbm2ddl.auto", hibernateDdlAuto);
+
+        ENTITY_MANAGER_FACTORY_INSTANCE = Persistence.createEntityManagerFactory("weatherPersistenceUnit", config);
     }
 
     public static EntityManagerFactory getEntityManagerFactory() {
