12ee67382c232d717219707ff18e34ee9cb44d0e
Vladimir Kidyaev <krios2146@gmail.com>
2023-04-30 18:09:55 +0700

    refactor(servlet): SearchServlet now throws  exceptions instead of processing error templates

--- src/main/java/pet/project/servlet/SearchServlet.java
+++ src/main/java/pet/project/servlet/SearchServlet.java
@@ -7,6 +7,10 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import pet.project.dao.LocationDao;
 import pet.project.dao.SessionDao;
+import pet.project.exception.CookieNotFoundException;
+import pet.project.exception.InvalidParameterException;
+import pet.project.exception.SessionExpiredException;
+import pet.project.exception.api.GeocodingApiCallException;
 import pet.project.model.Location;
 import pet.project.model.Session;
 import pet.project.model.User;
@@ -26,108 +30,73 @@ public class SearchServlet extends WeatherTrackerBaseServlet {
     private final WeatherApiService weatherApiService = new WeatherApiService();
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, CookieNotFoundException, SessionExpiredException, InvalidParameterException, GeocodingApiCallException {
         log.info("Finding cookie with session id");
         Cookie[] cookies = req.getCookies();
-        Optional<Cookie> cookieOptional = findCookieByName(cookies, "sessionId");
+        Cookie cookie = findCookieByName(cookies, "sessionId")
+                .orElseThrow(() -> new CookieNotFoundException("Cookie with session id is not found"));
 
-        if (cookieOptional.isEmpty()) {
-            log.info("Cookie is not found: processing empty home page");
-            context.clearVariables();
-            templateEngine.process("home", context, resp.getWriter());
-            return;
-        }
+        UUID sessionId = UUID.fromString(cookie.getValue());
 
-        log.info("Finding session from cookie");
-        UUID sessionId = UUID.fromString(cookieOptional.get().getValue());
-        Optional<Session> sessionOptional = sessionDao.findById(sessionId);
+        log.info("Finding session: " + sessionId);
+        Session session = sessionDao.findById(sessionId)
+                .orElseThrow(() -> new SessionExpiredException("Session with id " + sessionId + "has expired"));
 
-        if (sessionOptional.isEmpty()) {
-            log.info("Session has expired: redirecting to the sign-in page");
-            resp.sendRedirect(req.getContextPath() + "/sign-in");
-            return;
-        }
-
-        User user = sessionOptional.get().getUser();
+        User user = session.getUser();
 
         String searchQuery = req.getParameter("q");
 
         if (searchQuery == null || searchQuery.isBlank()) {
-            log.warn("Search query is invalid: processing error page");
-            templateEngine.process("error", context);
-            return;
+            throw new InvalidParameterException("Search query is invalid");
         }
 
-        try {
-            log.info("Calling openweather geocoding API");
-            List<LocationApiResponse> foundLocations = weatherApiService.getLocationsByName(searchQuery);
+        log.info("Calling openweather geocoding API: " + searchQuery);
+        List<LocationApiResponse> foundLocations = weatherApiService.getLocationsByName(searchQuery);
 
-            context.setVariable("login", user.getLogin());
-            context.setVariable("foundLocations", foundLocations);
+        context.setVariable("login", user.getLogin());
+        context.setVariable("foundLocations", foundLocations);
 
-            log.info("Processing search page");
-            templateEngine.process("search", context, resp.getWriter());
-
-        } catch (InterruptedException e) {
-            log.warn("Issues with geocoding api call");
-            templateEngine.process("error", context, resp.getWriter());
-        }
+        log.info("Processing search page");
+        templateEngine.process("search", context, resp.getWriter());
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, CookieNotFoundException, SessionExpiredException, InvalidParameterException {
         log.info("Finding cookie with session id");
         Cookie[] cookies = req.getCookies();
-        Optional<Cookie> cookieOptional = findCookieByName(cookies, "sessionId");
+        Cookie cookie = findCookieByName(cookies, "sessionId")
+                .orElseThrow(() -> new CookieNotFoundException("Cookie with session id is not found"));
 
-        if (cookieOptional.isEmpty()) {
-            log.info("Cookie is not found: processing empty home page");
-            context.clearVariables();
-            templateEngine.process("home", context, resp.getWriter());
-            return;
-        }
+        UUID sessionId = UUID.fromString(cookie.getValue());
 
-        log.info("Finding session from cookie");
-        UUID sessionId = UUID.fromString(cookieOptional.get().getValue());
-        Optional<Session> sessionOptional = sessionDao.findById(sessionId);
+        log.info("Finding session: " + sessionId);
+        Session session = sessionDao.findById(sessionId)
+                .orElseThrow(() -> new SessionExpiredException("Session with id " + sessionId + "has expired"));
 
-        if (sessionOptional.isEmpty()) {
-            log.info("Session has expired: redirecting to the sign-in page");
-            resp.sendRedirect(req.getContextPath() + "/sign-in");
-            return;
-        }
-
-        User user = sessionOptional.get().getUser();
+        User user = session.getUser();
 
         String name = req.getParameter("name");
-        if (name == null || name.isBlank()) {
-            log.warn("Parameter name is invalid");
-            templateEngine.process("error", context);
-            return;
-        }
-
         String latitudeParam = req.getParameter("latitude");
-        if (latitudeParam == null || latitudeParam.isBlank()) {
-            log.warn("Parameter latitude is invalid");
-            templateEngine.process("error", context);
-            return;
-        }
-
         String longitudeParam = req.getParameter("longitude");
+
+        if (name == null || name.isBlank()) {
+            throw new InvalidParameterException("Parameter name is invalid");
+        }
+        if (latitudeParam == null || latitudeParam.isBlank()) {
+            throw new InvalidParameterException("Parameter latitude is invalid");
+        }
         if (longitudeParam == null || longitudeParam.isBlank()) {
-            log.warn("Parameter longitude is invalid");
-            templateEngine.process("error", context);
-            return;
+            throw new InvalidParameterException("Parameter longitude is invalid");
         }
 
         Double latitude = Double.valueOf(latitudeParam);
         Double longitude = Double.valueOf(longitudeParam);
 
-        log.info("Finding location by coordinates");
+        log.info("Finding location: lat=" + latitude + " lon=" + longitude);
         Optional<Location> locationOptional = locationDao.findByCoordinates(latitude, longitude);
 
         if (locationOptional.isPresent()) {
-            log.info("Location is found: adding current user to location users");
+            log.info("Location is found: " + locationOptional.get().getId() + " Adding user: " + user.getId() + " to location: " + locationOptional.get().getId());
             Location location = locationOptional.get();
 
             List<User> users = location.getUsers();
@@ -146,7 +115,7 @@ public class SearchServlet extends WeatherTrackerBaseServlet {
             locationDao.save(location);
         }
 
-        log.info("Adding location to tracked list is successful: redirecting to the home page");
+        log.info("Adding location to the tracked list is successful: redirecting to the home page");
         resp.sendRedirect(req.getContextPath());
     }
 }
