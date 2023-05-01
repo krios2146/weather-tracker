6d18bca6ebe400b561273d45bfbc51e2b059b66a
Vladimir Kidyaev <krios2146@gmail.com>
2023-05-01 12:02:31 +0700

    fix(servlet): context now is build properly in the base servlet

--- src/main/java/pet/project/servlet/WeatherTrackerBaseServlet.java
+++ src/main/java/pet/project/servlet/WeatherTrackerBaseServlet.java
@@ -40,7 +40,7 @@ public abstract class WeatherTrackerBaseServlet extends HttpServlet {
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         if (context == null) {
             log.debug("Context is null: building");
-            ThymeleafUtil.buildWebContext(req, resp, getServletContext());
+            context = ThymeleafUtil.buildWebContext(req, resp, getServletContext());
         }
 
         try {
