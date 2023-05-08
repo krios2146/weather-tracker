156bf6b552615f321f21403adb3ba5fff1028775
Vladimir Kidyaev <krios2146@gmail.com>
2023-05-08 20:49:44 +0700

    feat: add ThymeleafContextListener

---
+++ src/main/java/pet/project/ThymeleafContextListener.java
@@ -0,0 +1,20 @@
+package pet.project;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import org.thymeleaf.TemplateEngine;
+import pet.project.util.ThymeleafUtil;
+
+@WebListener
+public class ThymeleafContextListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        ServletContext servletContext = sce.getServletContext();
+
+        TemplateEngine templateEngine = ThymeleafUtil.buildTemplateEngine(servletContext);
+
+        servletContext.setAttribute("templateEngine", templateEngine);
+    }
+}
