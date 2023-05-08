4abfb22e8b94f8f1314b01dd799fcf0d3b1451bd
Vladimir Kidyaev <krios2146@gmail.com>
2023-05-08 20:50:47 +0700

    refactor: ThymeleafUtil no more has singleton template engine object

--- src/main/java/pet/project/util/ThymeleafUtil.java
+++ src/main/java/pet/project/util/ThymeleafUtil.java
@@ -3,7 +3,6 @@ package pet.project.util;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.thymeleaf.ITemplateEngine;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 import org.thymeleaf.templatemode.TemplateMode;
@@ -14,26 +13,20 @@ import org.thymeleaf.web.servlet.IServletWebExchange;
 import org.thymeleaf.web.servlet.JakartaServletWebApplication;
 
 public class ThymeleafUtil {
-    private static final TemplateEngine TEMPLATE_ENGINE_INSTANCE = new TemplateEngine();
-
-    private ThymeleafUtil() {
-
-    }
-
     public static WebContext buildWebContext(HttpServletRequest req, HttpServletResponse resp, ServletContext servletContext) {
         JakartaServletWebApplication application = JakartaServletWebApplication.buildApplication(servletContext);
         IServletWebExchange webExchange = application.buildExchange(req, resp);
         return new WebContext(webExchange);
     }
 
-    public static ITemplateEngine getTemplateEngine() {
-        return TEMPLATE_ENGINE_INSTANCE;
-    }
-
-    public static void buildTemplateEngine(ServletContext context) {
+    public static TemplateEngine buildTemplateEngine(ServletContext context) {
         IWebApplication application = JakartaServletWebApplication.buildApplication(context);
         ITemplateResolver templateResolver = buildTemplateResolver(application);
-        TEMPLATE_ENGINE_INSTANCE.setTemplateResolver(templateResolver);
+
+        TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+
+        return templateEngine;
     }
 
     private static ITemplateResolver buildTemplateResolver(IWebApplication application) {
