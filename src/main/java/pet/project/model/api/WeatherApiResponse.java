714a61a710e2c2b9900e2b76373638adb785a9f7
Vladimir Kidyaev <krios2146@gmail.com>
2023-05-10 13:15:50 +0700

    refactor(model): extract nested classes from WeatherApiResponse model

--- src/main/java/pet/project/model/api/WeatherApiResponse.java
+++ src/main/java/pet/project/model/api/WeatherApiResponse.java
@@ -2,15 +2,15 @@ package pet.project.model.api;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Getter;
+import pet.project.model.api.entity.Clouds;
+import pet.project.model.api.entity.Main;
+import pet.project.model.api.entity.Weather;
+import pet.project.model.api.entity.Wind;
+import pet.project.model.api.util.UnixTimestampDeserializer;
 
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -29,94 +29,21 @@ public class WeatherApiResponse {
     private Clouds clouds;
 
     @JsonProperty("dt")
-    @JsonDeserialize(using = UnixDateTimestampDeserializer.class)
-    private String date;
+    @JsonDeserialize(using = UnixTimestampDeserializer.class)
+    private LocalDateTime date;
 
     @JsonProperty("sys")
     private Sys sys;
 
     @Getter
     @JsonIgnoreProperties(ignoreUnknown = true)
-    public static class Weather {
-        @JsonProperty("id")
-        private Integer id;
-
-        @JsonProperty("main")
-        private String currentState;
-
-        @JsonProperty("description")
-        private String description;
-    }
-
-    @Getter
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    public static class Main {
-        @JsonProperty("temp")
-        private Double temperature;
-
-        @JsonProperty("feels_like")
-        private Double temperatureFeelsLike;
-
-        @JsonProperty("pressure")
-        private Integer pressure;
-
-        @JsonProperty("humidity")
-        private Integer humidity;
-
-        @JsonProperty("temp_min")
-        private Double temperatureMinimal;
-
-        @JsonProperty("temp_max")
-        private Double temperatureMaximum;
-    }
-
-    @Getter
-    public static class Wind {
-        @JsonProperty("speed")
-        private Double speed;
-
-        @JsonProperty("deg")
-        private Integer deg;
-
-        @JsonProperty("gust")
-        private Double gust;
-    }
-
-    @Getter
-    public static class Clouds {
-        @JsonProperty("all")
-        private Integer cloudiness;
-    }
-
-    @Getter
-    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Sys {
         @JsonProperty("sunrise")
-        @JsonDeserialize(using = UnixTimeTimestampDeserializer.class)
-        private String sunriseTime;
+        @JsonDeserialize(using = UnixTimestampDeserializer.class)
+        private LocalDateTime sunriseTime;
 
         @JsonProperty("sunset")
-        @JsonDeserialize(using = UnixTimeTimestampDeserializer.class)
-        private String sunsetTime;
-    }
-
-    private static class UnixDateTimestampDeserializer extends JsonDeserializer<String> {
-        @Override
-        public String deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
-            long timestamp = jsonParser.getValueAsLong();
-            Date date = new Date(timestamp * 1000);
-            SimpleDateFormat formatter = new SimpleDateFormat("dd-MM HH:mm");
-            return formatter.format(date);
-        }
-    }
-
-    private static class UnixTimeTimestampDeserializer extends JsonDeserializer<String> {
-        @Override
-        public String deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
-            long timestamp = jsonParser.getValueAsLong();
-            Date date = new Date(timestamp * 1000);
-            SimpleDateFormat formatter = new SimpleDateFormat("HH:mm");
-            return formatter.format(date);
-        }
+        @JsonDeserialize(using = UnixTimestampDeserializer.class)
+        private LocalDateTime sunsetTime;
     }
 }
