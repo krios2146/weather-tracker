2b9e39f08f3b8f9843ad9ddbf1046e55d7282490
Vladimir Kidyaev <krios2146@gmail.com>
2023-05-12 12:45:58 +0700

    feat(test): add tests for `doPost` method to the HomeServletTest

--- src/test/java/pet/project/servlet/HomeServletTest.java
+++ src/test/java/pet/project/servlet/HomeServletTest.java
@@ -13,6 +13,8 @@ import org.thymeleaf.context.WebContext;
 import pet.project.dao.LocationDao;
 import pet.project.dao.SessionDao;
 import pet.project.exception.CookieNotFoundException;
+import pet.project.exception.InvalidParameterException;
+import pet.project.exception.LocationNotFoundException;
 import pet.project.exception.SessionExpiredException;
 import pet.project.model.Location;
 import pet.project.model.Session;
@@ -26,10 +28,13 @@ import pet.project.service.WeatherApiService;
 
 import java.lang.reflect.Field;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -92,6 +97,7 @@ class HomeServletTest {
     public void doGet_sessionNotFound_shouldThrowSessionExpiredException() {
         when(request.getCookies()).thenReturn(new Cookie[]{new Cookie("sessionId", UUID.randomUUID().toString())});
 
+        verify(sessionDao, atMostOnce()).findById(any());
         assertThrows(
                 SessionExpiredException.class,
                 () -> homeServlet.doGet(request, response)
@@ -104,7 +110,7 @@ class HomeServletTest {
         Session session = new Session();
         session.setExpiresAt(LocalDateTime.MIN);
         when(request.getCookies()).thenReturn(new Cookie[]{cookie});
-        lenient().when(sessionDao.findById(any())).thenReturn(Optional.of(session));
+        when(sessionDao.findById(any())).thenReturn(Optional.of(session));
 
         assertThrows(
                 SessionExpiredException.class,
@@ -152,5 +158,128 @@ class HomeServletTest {
 
         verify(context, times(2)).setVariable(anyString(), any());
         verify(templateEngine, atMostOnce()).process(eq("home"), eq(context), any());
+    }
+
+    @Test
+    public void doPost_emptyCookies_shouldThrowCookieNotFoundException() {
+        when(request.getCookies()).thenReturn(new Cookie[]{});
+
+        assertThrows(
+                CookieNotFoundException.class,
+                () -> homeServlet.doPost(request, response)
+        );
+    }
+
+    @Test
+    public void doPost_sessionNotFound_shouldThrowSessionExpiredException() {
+        when(request.getCookies()).thenReturn(new Cookie[]{new Cookie("sessionId", UUID.randomUUID().toString())});
+
+        verify(sessionDao, atMostOnce()).findById(any());
+        assertThrows(
+                SessionExpiredException.class,
+                () -> homeServlet.doPost(request, response)
+        );
+    }
+
+    @Test
+    public void doPost_sessionExpired_shouldThrowSessionExpiredException() {
+        Cookie cookie = new Cookie("sessionId", UUID.randomUUID().toString());
+        Session session = new Session();
+        session.setExpiresAt(LocalDateTime.MIN);
+        when(request.getCookies()).thenReturn(new Cookie[]{cookie});
+        when(sessionDao.findById(any())).thenReturn(Optional.of(session));
+
+        assertThrows(
+                SessionExpiredException.class,
+                () -> homeServlet.doPost(request, response)
+        );
+    }
+
+    @Test
+    public void doPost_locationIdParamIsNull_shouldThrowInvalidParameterException() {
+        Cookie cookie = new Cookie("sessionId", UUID.randomUUID().toString());
+        Session session = new Session();
+        session.setExpiresAt(LocalDateTime.MAX);
+        when(request.getCookies()).thenReturn(new Cookie[]{cookie});
+        when(sessionDao.findById(any())).thenReturn(Optional.of(session));
+
+        verify(locationDao, never()).findById(any());
+        assertThrows(
+                InvalidParameterException.class,
+                () -> homeServlet.doPost(request, response)
+        );
+    }
+
+    @Test
+    public void doPost_locationIdParamIsBlank_shouldThrowInvalidParameterException() {
+        Cookie cookie = new Cookie("sessionId", UUID.randomUUID().toString());
+        Session session = new Session();
+        session.setExpiresAt(LocalDateTime.MAX);
+        when(request.getCookies()).thenReturn(new Cookie[]{cookie});
+        when(sessionDao.findById(any())).thenReturn(Optional.of(session));
+        when(request.getParameter(eq("locationId"))).thenReturn(" ");
+
+        verify(locationDao, never()).findById(any());
+        assertThrows(
+                InvalidParameterException.class,
+                () -> homeServlet.doPost(request, response)
+        );
+    }
+
+    @Test
+    public void doPost_locationNotFound_shouldThrowLocationNotFoundException() {
+        Cookie cookie = new Cookie("sessionId", UUID.randomUUID().toString());
+        Session session = new Session();
+        session.setExpiresAt(LocalDateTime.MAX);
+        when(request.getCookies()).thenReturn(new Cookie[]{cookie});
+        when(sessionDao.findById(any())).thenReturn(Optional.of(session));
+        when(request.getParameter(any())).thenReturn("1");
+
+        verify(locationDao, atMostOnce()).findById(eq(1L));
+        assertThrows(
+                LocationNotFoundException.class,
+                () -> homeServlet.doPost(request, response)
+        );
+    }
+
+    @Test
+    public void doPost_locationFound_shouldDeleteUserFromLocation() throws Exception {
+        Cookie cookie = new Cookie("sessionId", UUID.randomUUID().toString());
+        User userToPersist = new User();
+        User userToDelete = new User();
+        Session session = new Session();
+        session.setExpiresAt(LocalDateTime.MAX);
+        session.setUser(userToDelete);
+        Location location = new Location();
+        location.setUsers(new ArrayList<>(asList(userToDelete, userToPersist)));
+        when(request.getCookies()).thenReturn(new Cookie[]{cookie});
+        when(sessionDao.findById(any())).thenReturn(Optional.of(session));
+        when(request.getParameter(any())).thenReturn("1");
+        when(locationDao.findById(eq(1L))).thenReturn(Optional.of(location));
+
+        homeServlet.doPost(request, response);
+
+        verify(locationDao, atMostOnce()).update(location);
+        assertEquals(List.of(userToPersist), location.getUsers());
+    }
+
+    @Test
+    public void doPost_deletingSuccessful_shouldRedirectToHomePage() throws Exception {
+        Cookie cookie = new Cookie("sessionId", UUID.randomUUID().toString());
+        User userToPersist = new User();
+        User userToDelete = new User();
+        Session session = new Session();
+        session.setExpiresAt(LocalDateTime.MAX);
+        session.setUser(userToDelete);
+        Location location = new Location();
+        location.setUsers(new ArrayList<>(asList(userToDelete, userToPersist)));
+        when(request.getCookies()).thenReturn(new Cookie[]{cookie});
+        when(sessionDao.findById(any())).thenReturn(Optional.of(session));
+        when(request.getParameter(any())).thenReturn("1");
+        when(locationDao.findById(eq(1L))).thenReturn(Optional.of(location));
+
+        homeServlet.doPost(request, response);
+
+        verify(response, atMostOnce()).sendRedirect(request.getContextPath());
     }
 }
